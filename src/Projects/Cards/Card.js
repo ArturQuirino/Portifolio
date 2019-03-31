d54b13c4ea96bb4e65accd5c2842a1cf2566fbd0
Artur Quirino <arturquirino6@gmail.com>
2019-03-31 09:58:20 -0300

    create cards for portifolio

---
+++ src/Projects/Cards/Card.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Card.css';
+
+function Card(props) {
+  const { title, website, sourceCode, description } = props;
+  return (
+    <div className="card">
+      <div className="card__cover">{title}</div>
+      <div className="card__content">
+        <span className="card__title">{description}</span>
+        <a href={website} className="card__button">Visit Website</a>
+        <a href={sourceCode} className="card__button">Source Code</a>
+      </div>
+    </div>
+  );
+}
+
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+  website: PropTypes.string.isRequired,
+  sourceCode: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
+
+export default Card;
--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import Card from './Cards/Card';
+import './Projects.css';
 
 function Projects() {
-  return <div className="Projects">Projetos</div>;
+  return (
+    <div className="projects">
+      <Card
+        title="Artur's Saga"
+        website="google.com"
+        sourceCode="google.com"
+        description="A game where you need to help Artur to get his coffee and avoid bugs."
+      />
+    </div>
+  );
 }
 
 export default Projects;
